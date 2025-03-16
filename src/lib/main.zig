a3bbabdbd401e27e8eb6275db121bc84e924729e
zewenn <takaczsoli@gmail.com>
2025-03-16 18:41:36 +0100

    chore: bump version to v0.2.0b

--- src/lib/main.zig
+++ src/lib/main.zig
@@ -13,7 +13,7 @@ var random: std.Random = undefined;
 // --------------------------------------------------------------------------------
 pub const lib_info = struct {
     pub const lib_name = "fyr";
-    pub const version_str = "v0.1.0";
+    pub const version_str = "0.2.0b";
     pub const build_mode = builtin.mode;
 };
 
