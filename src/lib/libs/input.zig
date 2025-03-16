a23fb1b01b2af8bf3cd2a949e617753c2225a1c4
zewenn <takaczsoli@gmail.com>
2025-03-16 17:02:43 +0100

    new(input): added bindings with the corresponding Unity-like name to all raylib keyboard and mouse functions

---
+++ src/lib/libs/input.zig
@@ -0,0 +1,14 @@
+const fyr = @import("fyr");
+const rl = fyr.rl;
+
+pub const getKeyDown = rl.isKeyPressed;
+pub const getKeyDownRepeat = rl.isKeyPressedRepeat;
+pub const getKeyUp = rl.isKeyReleased;
+pub const getKey = rl.isKeyDown;
+pub const getKeyReleased = rl.isKeyUp;
+pub const getKeyPressed = rl.getKeyPressed();
+
+pub const getMouseDown = rl.isMouseButtonPressed;
+pub const getMouseUp = rl.isMouseButtonReleased;
+pub const getMouse = rl.isMouseButtonDown;
+pub const getMouseReleased = rl.isMouseButtonUp;
